1bd8a102c705a905e1a1716fee999f9868c6f930
polinaburaeva00-source <polinaburaeva00@gmail.com>
2025-10-10 19:56:40 +0300

    Add files via upload

---
+++ pr4/season.java
@@ -0,0 +1,42 @@
+package pr4;
+
+public enum season {
+    WINTER(-5) {
+        @Override
+        public String getDescription() {
+            return "Холодное время года";
+        }
+    },
+    SPRING(10) {
+        @Override
+        public String getDescription() {
+            return "Холодное время года";
+        }
+    },
+    SUMMER(25) {
+        @Override
+        public String getDescription() {
+            return "Теплое время года";
+        }
+    },
+    AUTUMN(15) {
+        @Override
+        public String getDescription() {
+            return "Холодное время года";
+        }
+    };
+
+    private final int averageTemperature;
+
+    season(int averageTemperature) {
+        this.averageTemperature = averageTemperature;
+    }
+
+    public int getAverageTemperature() {
+        return averageTemperature;
+    }
+
+    public String getDescription() {
+        return "Холодное время года";
+    }
+}
---
+++ pr4/seasonDescription.java
@@ -0,0 +1,31 @@
+package pr4;
+
+public class seasonDescription {
+    public static void describeSeason(season season) {
+        switch (season) {
+            case SUMMER:
+                System.out.println("Я люблю лето");
+                break;
+            case WINTER:
+                System.out.println("Я люблю зиму");
+                break;
+            case SPRING:
+                System.out.println("Я люблю весну");
+                break;
+            case AUTUMN:
+                System.out.println("Я люблю осень");
+                break;
+            default:
+                System.out.println("Неизвестное время года");
+        }
+    }
+
+    public static void printAllSeasons() {
+        for (season season : season.values()) {
+            System.out.println("Время года: " + season);
+            System.out.println("Средняя температура: " + season.getAverageTemperature());
+            System.out.println("Описание: " + season.getDescription());
+            System.out.println();
+        }
+    }
+}
